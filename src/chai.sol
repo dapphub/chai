3493e370cda2d7f761d38c98521a84a5a8fae87f
Lucas Vogelsang <l@lucasvo.com>
2019-11-19 23:39:23 +0000

    You're going to have to wait for your chai

--- src/chai.sol
+++ src/chai.sol
@@ -37,13 +37,6 @@ contract GemLike {
 }
 
 contract Chai is DSNote {
-    // --- Auth ---
-    mapping (address => uint) public wards;
-    function rely(address guy) external note auth { wards[guy] = 1; }
-    function deny(address guy) external note auth { wards[guy] = 0; }
-    modifier auth { require(wards[msg.sender] == 1); _; }
-
-
     // --- Data ---
     VatLike  public vat;
     PotLike  public pot;
@@ -54,7 +47,7 @@ contract Chai is DSNote {
     string  public constant name     = "Chai";
     string  public constant symbol   = "CHAI";
     string  public constant version  = "1";
-    uint8   public constant decimals = 18;
+    uint8   public constant decimals = 45;
     uint256 public totalSupply;
 
     mapping (address => uint)                      public balanceOf;
@@ -81,12 +74,15 @@ contract Chai is DSNote {
 
     // --- EIP712 niceties ---
     bytes32 public DOMAIN_SEPARATOR;
-    bytes32 public constant PERMIT_TYPEHASH = keccak256(
-        "Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)"
-    );
+    // keccak256("Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)"));
+    bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
+    // keccak256("Join(address usr,uint256 wad,uint256 fee,uint256 nonce,uint256 expiry)"));
+    bytes32 public constant JOIN_TYPEHASH = 0x701f8b62cdcbd2bd6d6bd1865186d960c371ab604e92406ac3d6a22ad8ed0547;
+    // keccak256("Exit(address usr,uint256 wad,uint256 fee,uint256 nonce,uint256 expiry)"));
+    bytes32 public constant EXIT_TYPEHASH = 0xab009dd688ed654007de4a45b77e1314a1efa28c92cfd01f6c132d967f3a27da;
+
 
     constructor(uint256 chainId_, address vat_, address join_, address pot_, address dai_) public {
-        wards[msg.sender] = 1;
         DOMAIN_SEPARATOR = keccak256(abi.encode(
             keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
             keccak256(bytes(name)),
@@ -122,34 +118,6 @@ contract Chai is DSNote {
         emit Transfer(src, dst, wad);
         return true;
     }
-
-    // wad is denominated in dai
-    function join(address usr, uint wad) external auth {
-        dai.transferFrom(msg.sender, address(this), wad);
-
-        daiJoin.join(address(this), wad);
-        uint pie = div(mul(ONE, wad), pot.chi());
-        pot.join(pie);
-
-        balanceOf[usr] = add(balanceOf[usr], pie);
-        totalSupply    = add(totalSupply, pie);
-        emit Transfer(address(0), usr, pie);
-    }
-
-    // wad is denominated in pie
-    function exit(address usr, uint wad) external {
-        require(balanceOf[usr] >= wad, "chai/insufficient-balance");
-        if (usr != msg.sender && allowance[usr][msg.sender] != uint(-1)) {
-            require(allowance[usr][msg.sender] >= wad, "chai/insufficient-allowance");
-            allowance[usr][msg.sender] = sub(allowance[usr][msg.sender], wad);
-        }
-        balanceOf[usr] = sub(balanceOf[usr], wad);
-        totalSupply    = sub(totalSupply, wad);
-
-        pot.exit(wad);
-        daiJoin.exit(msg.sender, div(mul(pot.chi(), wad), ONE));
-        emit Transfer(usr, address(0), wad);
-    }
     function approve(address usr, uint wad) external returns (bool) {
         allowance[msg.sender][usr] = wad;
         emit Approval(msg.sender, usr, wad);
@@ -167,7 +135,79 @@ contract Chai is DSNote {
         transferFrom(src, dst, wad);
     }
 
-    // --- Approve by signature ---
+
+    // --- Magic ---
+    // wad is denominated in dai
+    function join(address usr, uint wad) internal {
+        dai.transferFrom(usr, address(this), wad);
+
+        daiJoin.join(address(this), wad);
+        uint pie = div(mul(ONE, wad), pot.chi());
+        pot.join(pie);
+
+        balanceOf[usr] = add(balanceOf[usr], pie);
+        totalSupply    = add(totalSupply, pie);
+        emit Transfer(address(0), usr, pie);
+    }
+    function join(uint wad) public {
+        join(msg.sender, wad);
+    }
+    function join(address usr, uint wad, uint fee, uint nonce, uint expiry, bytes32 r, bytes32 s, uint8 v, address taxman) public {
+         bytes32 digest =
+            keccak256(abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(JOIN_TYPEHASH,
+                                     usr,
+                                     wad,
+                                     fee,
+                                     nonce,
+                                     expiry))
+        ));
+        require(usr == ecrecover(digest, v, r, s), "chai/invalid-permit");
+        require(expiry == 0 || now <= expiry, "chai/permit-expired");
+        require(nonce == nonces[usr]++, "chai/invalid-nonce");
+        join(usr, wad);
+        dai.transferFrom(usr, taxman, fee);
+    }
+
+    // wad is denominated in pie
+    function exit(address usr, uint wad) internal {
+        require(balanceOf[usr] >= wad, "chai/insufficient-balance");
+        if (usr != msg.sender && allowance[usr][msg.sender] != uint(-1)) {
+            require(allowance[usr][msg.sender] >= wad, "chai/insufficient-allowance");
+            allowance[usr][msg.sender] = sub(allowance[usr][msg.sender], wad);
+        }
+        balanceOf[usr] = sub(balanceOf[usr], wad);
+        totalSupply    = sub(totalSupply, wad);
+
+        pot.exit(wad);
+        daiJoin.exit(msg.sender, div(mul(pot.chi(), wad), ONE));
+        emit Transfer(usr, address(0), wad);
+    }
+    function exit(uint wad) public {
+        exit(msg.sender, wad);
+    }
+    function exit(address usr, uint wad, uint fee, uint nonce, uint expiry,
+                  bytes32 r, bytes32 s, uint8 v, address taxman)  public {
+         bytes32 digest =
+            keccak256(abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(EXIT_TYPEHASH,
+                                     usr,
+                                     wad,
+                                     fee,
+                                     nonce,
+                                     expiry))
+        ));
+        require(usr == ecrecover(digest, v, r, s), "chai/invalid-permit");
+        require(expiry == 0 || now <= expiry, "chai/permit-expired");
+        require(nonce == nonces[usr]++, "chai/invalid-nonce");
+        exit(usr, wad);
+        dai.transferFrom(usr, taxman, fee);
+    }
+
     function permit(address holder, address spender, uint256 nonce, uint256 expiry,
                     bool allowed, uint8 v, bytes32 r, bytes32 s) external
     {
